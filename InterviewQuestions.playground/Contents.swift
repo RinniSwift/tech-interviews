466bbe4ac8324f43752cfa6826837bed39bf9e74
Rinni Swift <rinradaswift@gmail.com>
2019-01-09 21:06:23 -0800

    find all the nodes that dont have a parent and all the nodes that have only one parent

--- InterviewQuestions.playground/Contents.swift
+++ InterviewQuestions.playground/Contents.swift
@@ -8,3 +8,64 @@
  
  */
 
+/*
+ 
+ all parents are on the 0 index
+ all children are on the 1 index
+ 
+ keep a count on all the parents found
+ keep a count on all the children found
+ if the parent shows up once, and is not contained in the child dict, that means there are no parents of that node
+ if the child value count is 1, that means that that child contains 1 parent
+ 
+ 
+ parArr: [1: 1, 2: 2, 3: 1]
+ childArr: [2: 1, 5: 1, 6: 1, 8: 1]
+ 
+ */
+
+
+
+
+func returnNoParAndOnePar(arr: [[Int]]) -> ([Int], [Int]) {
+    
+    var parDict = [Int: Int]()
+    var childDict = [Int: Int]()
+    
+    var noParArr = [Int]()
+    var oneParArr = [Int]()
+    
+    for pair in arr {
+        for (index, item) in pair.enumerated() {
+            if index == 0 {
+                var count = 1
+                if parDict[item] != nil {
+                    count += 1
+                    parDict[item] = count
+                } else {
+                    parDict[item] = count
+                }
+            } else if index == 1 {
+                var count = 1
+                if childDict[item] != nil {
+                    count += 1
+                    childDict[item] = count
+                } else {
+                    childDict[item] = count
+                }
+            }
+        }
+    }
+    for dict in parDict {
+        if !childDict.keys.contains(dict.key) {
+            noParArr.append(dict.key)
+        }
+    }
+    for dict in childDict {
+        if dict.value == 1 {
+            oneParArr.append(dict.key)
+        }
+    }
+    return (noParArr, oneParArr)
+}
+print(returnNoParAndOnePar(arr: [[1,2], [2, 5], [2, 6], [3,8], [9, 1]]))
