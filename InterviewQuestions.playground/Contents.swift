dce493ed09037ce5f6c6ffe3aeec5ccef2a3b5f5
Rinni Swift <rinradaswift@gmail.com>
2019-01-09 17:55:31 -0800

    more complex questions

---
+++ InterviewQuestions.playground/Contents.swift
@@ -0,0 +1,10 @@
+/*
+ 
+ Given an array of arrays, return the tuple of numbers that have no parents and that has only one parent
+ [[1,2], [2, 5], [2, 6], [3,8]]     ->      ([1, 3], [2, 5, 6, 8])
+ return value:
+ ([nodes that have no parents], [nodes that have only one parent])
+ ([         1, 3             ]  [           2, 5, 6, 8          ])
+ 
+ */
+
