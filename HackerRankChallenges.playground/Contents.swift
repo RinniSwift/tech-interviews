de9a6c0d32edecedc44f0d67f72b97b0d73b06b3
Rinni Swift <rinradaswift@gmail.com>
2019-01-19 12:16:28 -0800

    gathered hackerrank challenges and updated the interview questions playground

--- HackerRankChallenges.playground/Contents.swift
+++ HackerRankChallenges.playground/Contents.swift
@@ -1,3 +1,47 @@
-import UIKit
+/*
+ 
+ find the diagonal difference in a matrix from left to right and right to left
+ e.g. [[1, 2, 3],
+       [4, 5, 6],
+       [7, 8, 9]]          the difference will be: |(1 + 5 + 9) - (3 + 5 + 7)|
+ 
+ */
+func diagonalDifference(matrix: [[Int]]) {
+    
+}
+diagonalDifference(matrix: [[1, 2, 3],
+                            [4, 5, 6],
+                            [7, 8, 9]])
 
-var str = "Hello, playground"
+
+/*
+ 
+ Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+ arr = [1, 1, 0, -1, -1]
+ returns 0.400000 fraction of positive number
+ 0.400000 fraction of negative number
+ 0.200000 fraction of number zero
+ 
+ */
+func ratiosOfNumbers(numbers: [Int]) {
+    
+}
+ratiosOfNumbers(numbers: [1, 1, 0, -1, -1])
+
+
+/*
+ 
+ create a function that prints out the staircase
+ n = 6
+      #
+     ##
+    ###
+   ####
+  #####
+ ######
+ 
+ */
+func staircase(n: Int) {
+    
+}
+staircase(n: 6)
