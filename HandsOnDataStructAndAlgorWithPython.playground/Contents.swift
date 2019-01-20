005d9be79a859c9ae57c2a3213fe36cbf6ed0fdf
Rinni Swift <rinradaswift@gmail.com>
2019-01-19 21:02:31 -0800

    snippets from Hands on Datastructures and Algorithms With Python book

---
+++ HandsOnDataStructAndAlgorWithPython.playground/Contents.swift
@@ -0,0 +1,58 @@
+/*
+ 
+ create a function called yeild that basically does the loop through numbers from n to m
+ 
+ func yeild(_ n: Int, _ m : Int) {
+     var en = n
+     while en < m {
+         en + 1
+     }
+ }
+ 
+ */
+
+/*
+ TUPLES
+ tuples are lists that cant be changed
+ tuples are immutable: cannot assign changement to items
+ tuples are immutable arrays; are static;
+ tuples are not sorted
+ tuples can be indexed
+ tuples can be better than arrays because arrays store 6 items asuming it will be modified. but a tuple will nver assume so it locks the amount of items in there.
+ tuples can be read
+ */
+
+/*
+ 
+ SETS
+ sets are dictionaries without a key
+ stores only unique values
+ Set<T>()
+ 
+ */
+
+/*
+ ITERABLES
+ anything that is a collection of items
+ dictionaries
+ strings
+ arrays
+ tuples
+ */
+
+/*
+ QUEUES
+ 
+ -- DEQUES (python)
+    iterable
+    any value that can be inserted or removed on either side. (insert, pop)
+ */
+
+/*
+ 
+ RECURSSION
+ write a recurssion function that prints all the possible arrangments of a string (string, number)
+
+*/
+
+
