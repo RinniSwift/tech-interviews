b2d5bae87a99648eed40ef4e68684ea59df14e66
Rinni Swift <rinradaswift@gmail.com>
2019-01-21 11:16:54 -0800

    palindromes with numbers. using the string approach and not using a string approach

--- CrackingTheCode.playground/Pages/Palindromes.xcplaygroundpage/Contents.swift
+++ CrackingTheCode.playground/Pages/Palindromes.xcplaygroundpage/Contents.swift
@@ -25,3 +25,49 @@ func isPalindrome(string: String) -> Bool {
     return false
 }
 isPalindrome(string: "hannah")
+
+
+
+/*
+ 
+   +++ check if a number is read the same backwards
+        implementing with the string format and without using a string
+ 
+ */
+
+func numberIsPalindrom(number: Int) -> Bool {
+    let stringNum = String(number)
+    if String(number) == String(stringNum.reversed()) {
+        return true
+    } else {
+        return false
+    }
+    /*
+     if number.description == String(number.description.reversed()) {
+        return true
+     } else {
+        return false
+     }
+    */
+}
+numberIsPalindrom(number: 1221)
+
+func numberIsPalindromeTwo(num: Int) -> Bool {
+    var number = num
+    var reversednumber = 0
+    while number > 0 {
+        let remainder = number % 10
+        reversednumber = (reversednumber * 10) + remainder
+        number = number / 10
+        
+    }
+    if reversednumber == num {
+        return true
+    } else {
+        return false
+    }
+    
+    
+}
+numberIsPalindromeTwo(num: 121)
+
