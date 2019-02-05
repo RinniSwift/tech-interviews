d0dadb6be21f5bf7eaec3576df835026aec583f6
Rinni Swift <rinradaswift@gmail.com>
2019-02-05 10:14:25 -0800

    new implementation of rotating an array

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,8 +1,6 @@
 /*
- 
- return the highest product of 3 numbers from an array
- [1, 5, 3, 6, 2]
- 
+                                                return the highest product of 3 numbers from an array
+                                                [1, 5, 3, 6, 2]
  */
 func returnHighestProduct(arr: [Int]) -> Int {
     var resultProuct = 1
@@ -17,11 +15,9 @@ func returnHighestProduct(arr: [Int]) -> Int {
 
 
 /*
- 
- given an array, rotate it to the right by k times
- [1,2,3,4,5,6,7], k = 3
- --> [5,6,7,1,2,3,4]
- 
+                                                 given an array, rotate it to the right by k times
+                                                 [1,2,3,4,5,6,7], k = 3
+                                                                  --> [5,6,7,1,2,3,4]
  */
 func rotateToRight(arr: [Int], k: Int) -> [Int] {
     let first = arr.dropLast(3)
@@ -29,5 +25,14 @@ func rotateToRight(arr: [Int], k: Int) -> [Int] {
     return Array(last + first)
 }
 rotateToRight(arr: [1,2,3,4,5,6,7], k: 3)
+
+func rotateRight(arr: inout [Int], k: Int) -> [Int] {
+    for _ in 0..<k {
+        arr.insert(arr.removeLast(), at: 0)
+    }
+    return arr
+}
+var nums = [1,2,3,4,5,6,7]
+rotateRight(arr: &nums, k: 3)
 
 
