3cd2f59a54252a773c57d5463e3d09727086cb51
Rinni Swift <rinradaswift@gmail.com>
2019-06-07 15:04:55 -0700

    added some update code

--- medianOfTwoArrays.playground/Contents.swift
+++ medianOfTwoArrays.playground/Contents.swift
@@ -6,6 +6,8 @@
  There are two sorted arrays nums1 and nums2 of size m and n respectively.
  Find the median of the two sorted arrays.
  You may assume nums1 and nums2 cannot be both empty.
+ e.g. [1, 2], [3, 4] -> (2 + 3) / 2 = 2.5
+      [1, 2], [3] -> 2
  
  
  1. - Restating the problem
@@ -33,3 +35,86 @@
 
 // 5. - Implementation
 
+func medianOfTwoArr(nums1: [Int], nums2: [Int]) -> Double? {
+    
+    let totalItemsCount = nums1.count + nums2.count
+    
+    if nums2.first! >= nums1.last! {
+        
+        let medianInd = totalItemsCount / 2
+        
+        if totalItemsCount.isMultiple(of: 2) {
+            // retrieve that index, and (index + 1)
+            guard medianInd < nums1.count else {
+                let indNextArr = medianInd - nums1.count
+                if indNextArr - 1 < 0 {
+                    return Double((nums2[indNextArr] + nums1.last!)) / 2
+                } else {
+                    return Double((nums2[indNextArr] + nums2[indNextArr + 1])) / 2
+                }
+            }
+            return Double(nums1[medianInd] / nums2[medianInd - 1])
+            
+        } else {
+            // retrieve straight to the index
+            guard medianInd < nums1.count else {
+                return Double(nums2[medianInd - nums1.count])
+            }
+            return Double(nums1[medianInd])
+        }
+    }
+    return nil
+}
+
+print(medianOfTwoArr(nums1: [1, 2], nums2: [3, 4, 5, 6]))
+
+
+
+func findMedianOfTwo(nums1: [Int], nums2: [Int]) -> Double? {
+    // combine two arrays together
+    var SortedComArr = [Int]()
+    let medianIndex = (nums1.count + nums2.count) / 2
+    let totalCount = nums1.count + nums2.count
+    
+    var pointer1 = 0
+    var pointer2 = 0
+    
+    guard !nums2.isEmpty || !nums1.isEmpty else {
+        if nums2.isEmpty {
+            return Double(nums1[medianIndex])
+        } else if nums1.isEmpty {
+            return Double(nums2[medianIndex])
+        }
+        return nil
+    }
+    
+    while SortedComArr.count != (nums2.count + nums1.count) {
+        guard pointer1 < nums1.count else {
+            continue
+        }
+        guard pointer2 < nums2.count else {
+            continue
+        }
+        
+        if nums1[pointer1] < nums2[pointer2] {
+            SortedComArr.append(nums1[pointer1])
+            pointer1 += 1
+        } else if nums1[pointer1] > nums2[pointer2] {
+            SortedComArr.append(nums2[pointer2])
+            pointer2 += 1
+        } else if nums1[pointer1] == nums2[pointer2] {
+            SortedComArr.append(nums1[pointer1])
+            SortedComArr.append(nums2[pointer2])
+            pointer1 += 1
+            pointer2 += 1
+        }
+    }
+
+    if totalCount.isMultiple(of: 2) {
+        return Double(SortedComArr[medianIndex] + SortedComArr[medianIndex + 1]) / 2
+    } else {
+        return Double(SortedComArr[medianIndex])
+    }
+}
+
+findMedianOfTwo(nums1: [1, 2], nums2: [3, 4])
