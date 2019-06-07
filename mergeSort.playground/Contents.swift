f4b10da701044d32843eccad5e41090f2bcda913
Rinni Swift <rinradaswift@gmail.com>
2019-06-07 16:59:33 -0700

    added new implementation

--- mergeSort.playground/Contents.swift
+++ mergeSort.playground/Contents.swift
@@ -46,3 +46,41 @@ func mergeNumber(arr: [Int], number: Int) -> [Int] {
 }
 
 mergeNumber(arr: [1, 5, 6, 9, 9, 10], number: 9)
+
+
+func mergeNumberBetter(arr: [Int], number: Int) -> [Int] {
+    var mergedArr = arr
+    var middle = arr.count / 2
+    
+    // check if less or greater than the bounds
+    if number <= arr.first! {
+        mergedArr.insert(number, at: 0)
+    } else if number >= arr.last! {
+        mergedArr.append(number)
+    }
+    
+    while middle > 0 && middle < arr.count {
+        if number < arr[middle] {
+            middle = middle / 2
+            if number > arr[middle] {
+                mergedArr.insert(number, at: middle + 1)
+                break
+            }
+        } else if number > arr[middle] {
+            middle = middle + (middle / 2)
+            if number < arr[middle] {
+                mergedArr.insert(number, at: middle)
+                break
+            }
+        }
+    }
+    
+    return mergedArr
+}
+
+print(mergeNumberBetter(arr: [1, 2, 4, 5, 6], number: 6))
+
+
+
+
+// 2. Merge multiple elements to the given sequence. with O(n (log n))
