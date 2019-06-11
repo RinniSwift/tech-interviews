d61c4651da05e8fa807ec1cfe1b48932510f2d9a
Rinni Swift <rinradaswift@gmail.com>
2019-06-11 13:05:07 -0700

    added 1 solution to merging number to sorted array

--- mergeSort.playground/Contents.swift
+++ mergeSort.playground/Contents.swift
@@ -37,9 +37,8 @@ func mergeIntoSorted(arr: [Int], number: Int) -> [Int] {
             if number > arr[medianInd - 1] || number == arr[medianInd] {
                 sorted.insert(number, at: medianInd)
                 break
-            } else {
-                medianInd = medianInd / 2
             }
+            medianInd = medianInd / 2
         } else {
             guard medianInd < arr.count else {
                 continue
@@ -47,108 +46,14 @@ func mergeIntoSorted(arr: [Int], number: Int) -> [Int] {
             if number < arr[medianInd + 1] || number == arr[medianInd] {
                 sorted.insert(number, at: medianInd)
                 break
-            } else {
-                medianInd = medianInd + (medianInd / 2)
             }
+            medianInd = medianInd + (medianInd / 2)
         }
     }
     
     return sorted
 }
 mergeIntoSorted(arr: [1, 3, 5, 7], number: 4)
-
-
-
-
-func mergeNumber(arr: [Int], number: Int) -> [Int] {
-    var mergedArr = arr
-    
-    if number <= arr.first! {
-        mergedArr.insert(number, at: 0)
-        return mergedArr
-    } else  if number >= arr.last! {
-        mergedArr.append(number)
-        return mergedArr
-    }
-    
-    var middle = arr.count / 2
-    
-    while middle > 0 || middle < arr.count {
-        if number < arr[middle] {
-            guard middle - 1 >= 0 else {
-                middle = middle / 2
-                continue
-            }
-            if number > arr[middle - 1] {
-                mergedArr.insert(number, at: middle)
-                break
-            } else {
-                middle = middle / 2
-            }
-        } else if number > arr[middle] {
-            guard middle + 1 < arr.count else {
-                middle = middle + (middle / 2)
-                continue
-            }
-            if number < arr[middle + 1] {
-                mergedArr.insert(number, at: middle + 1)
-                break
-            }
-        } else if number == arr[middle] {
-            mergedArr.insert(number, at: middle)
-            break
-        }
-    }
-    
-    return mergedArr
-}
-
-mergeNumber(arr: [1, 5, 6, 9, 9, 10], number: 9)
-
-
-func mergeNumberBetter(arr: [Int], number: Int) -> [Int] {
-    var mergedArr = arr
-    var middle = arr.count / 2
-    
-    // check if less or greater than the bounds
-    if number <= arr.first! {
-        mergedArr.insert(number, at: 0)
-        return mergedArr
-    } else if number >= arr.last! {
-        mergedArr.append(number)
-        return mergedArr
-    }
-    
-    while middle > 0 && middle < arr.count {
-        if number < arr[middle] {
-            middle = middle / 2
-            if number > arr[middle] {
-                mergedArr.insert(number, at: middle + 1)
-                break
-            } else if number == arr[middle] {
-                mergedArr.insert(number, at: middle)
-                break
-            }
-        } else if number > arr[middle] {
-            middle = middle + (middle / 2)
-            if number < arr[middle] {
-                mergedArr.insert(number, at: middle)
-                break
-            } else if number == arr[middle] {
-                mergedArr.insert(number, at: middle)
-                break
-            }
-        } else if number == arr[middle] {
-            mergedArr.insert(number, at: middle)
-            break
-        }
-    }
-    
-    return mergedArr
-}
-
-print(mergeNumberBetter(arr: [1, 2, 4, 5, 6], number: 6))
-
 
 
 
