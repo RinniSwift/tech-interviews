8405c0cf15b5135eb9cd0b4dcfc6bedc81b173df
Rinni Swift <rinradaswift@gmail.com>
2019-08-15 17:23:43 -0700

    update palindrome problem

--- longestPalindrome.playground/Contents.swift
+++ longestPalindrome.playground/Contents.swift
@@ -13,25 +13,81 @@ import UIKit
  Input: "cbbd"
  Output: "bb"
  
+ Clarifying questions:
+ . when checking, is it case sensitive
+ . is the input lower case
+ 
  */
 
-func longestPalindrome(from string: String) {
-    // 1. create a histogram
-    // 2. loop over the string
-    // 3. create a string, if there is more than two counts, add two of those characters to the string, decrement 2 out of the count histogram
-    // 4. if there is 1 count, skip the character.
+func longestPalindrome(from s: String) -> Substring {
+    // window frame approach
     
-    var histogram = [Character: Int]()
+    // start from s.startIndex,s.endIndex
+    // check the characters at each end, if they're not equal, shift the startIndex up
+    // if it can't find, loop again and shift the endIndex down.
     
-    for char in string {
-        if histogram[char] == nil {
-            histogram[char] = 1
+    var start = s.startIndex
+    var end = s.index(before: s.endIndex)
+    
+    var palindrome: Substring = ""
+    
+    
+    while start <= end {
+//        print("character at start: \(s[start]). character at end: \(s[end])")
+        if s[start] == s[end] {
+            print("EQUAL: character at start: \(s[start]). character at end: \(s[end])")
+
+            if start == end {
+                palindrome.insert(s[start], at: palindrome.index(palindrome.startIndex, offsetBy: palindrome.count / 2))
+                return palindrome
+            }
+
+            palindrome.insert(s[start], at: palindrome.index(palindrome.startIndex, offsetBy: palindrome.count / 2))
+            palindrome.insert(s[end], at: palindrome.index(palindrome.startIndex, offsetBy: palindrome.count / 2))
+            print(palindrome)
+            
+            start = s.index(after: start)
+            end = s.index(before: end)
+
         } else {
-            histogram[char]! += 1
+            print("NOT EQUAL: character at start: \(s[start]). character at end: \(s[end])")
+            
+            
+            start = s.index(after: start)
+            palindrome.removeAll()
+        }
+
+    }
+    
+    return ""
+    
+}
+longestPalindrome(from: "babbab")
+
+
+
+
+func isPalindrome(word: String) -> Bool {
+    
+    var startNum = 0
+    var endNum = word.count - 1
+    
+    while startNum <= (word.count / 2) && endNum >= (word.count / 2) {
+        let startInd = word.index(word.startIndex, offsetBy: startNum)
+        let endInd = word.index(word.startIndex, offsetBy: endNum)
+        
+        if word[startInd] == word[endInd] {
+            startNum += 1
+            endNum -= 1
+        } else {
+            return false
         }
     }
-    print(histogram)
+    
+    return true
 }
-longestPalindrome(from: "babad")
+isPalindrome(word: "rinnir")
+
+
 
 
