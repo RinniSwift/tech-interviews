e1f8f7d2068b3d2cb1234309587d5365440119a2
Rinni Swift <rinradaswift@gmail.com>
2019-08-16 08:26:46 -0700

    added recuressive and iterative function

--- fibonacci.playground/Contents.swift
+++ fibonacci.playground/Contents.swift
@@ -12,8 +12,8 @@ func fiboSequence(times: Int) -> [Int] {
     
     return seq
 }
+//print(fiboSequence(times: 10))
 
-print(fiboSequence(times: 10))
 
 // Function based iterator
 func fiboIterator() -> AnyIterator<Int> {
@@ -24,8 +24,9 @@ func fiboIterator() -> AnyIterator<Int> {
         return upcomingNumber
     }
 }
-let fibSequence = AnySequence(fiboIterator)
-Array(fibSequence.prefix(10))
+//let fibSequence = AnySequence(fiboIterator)
+//Array(fibSequence.prefix(10))
+
 
 // Function based sequence
 let fiboSequenceTwo = sequence(state: (0, 1)) {
@@ -34,7 +35,64 @@ let fiboSequenceTwo = sequence(state: (0, 1)) {
     state = (state.1, state.0 + state.1)
     return upcomingNumber
 }
-Array(fiboSequenceTwo.prefix(10))
+//Array(fiboSequenceTwo.prefix(10))
 
 
 
+/*
+ write a function that takes in a number, n, and returns the fibo sequence at that number
+ 
+ n: 2 : (0, 1) -> 1
+ n: 5 : (0, 1, 1, 2, 3) -> 3
+ 
+ Clarifying questions:
+    . I'm assuming that all iterations start at [0, 1] -- yes
+    . What if the input is something less than 0 -- return (-1)
+ 
+ */
+
+func fibo(n: Int) -> Int {
+    var fiboSeq = (0, 1)
+    
+    guard n > 1 else {
+        if n == 1 {
+            return 0
+        }
+        return -1
+    }
+    
+    for _ in 0..<n - 2 {
+        let sum = (fiboSeq.0 + fiboSeq.1)
+        fiboSeq = (fiboSeq.1, sum)
+        
+    }
+    
+    return fiboSeq.1
+}
+fibo(n: 5)
+
+
+
+/*
+ write a recurrsive function that takes in a number, n, and returns the fibo sequence at that number
+ 
+ n: 2 : (0, 1) -> 1
+ n: 5 : (0, 1, 1, 2, 3) -> 3
+ 
+ Clarifying questions:
+ . I'm assuming that all iterations start at [0, 1] -- yes
+ 
+ */
+
+func fiboReccur(n: Int) -> Int {
+    
+    if n == 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    }
+    
+    return fiboReccur(n: n - 1) + fiboReccur(n: n - 2)
+    
+}
+fiboReccur(n: 5)
