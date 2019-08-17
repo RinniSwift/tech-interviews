b4dbd5f86829489ad4bba4a4d35b0ebdb2b7302e
Rinni Swift <rinradaswift@gmail.com>
2019-08-16 20:28:41 -0700

    added compressing string question with initial solution

---
+++ encodeString.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+/*
+ 
+ given a string, encode the string to make it shorter and compressed
+ 
+ input: "aaabbcccc"
+ output: "a3b2c3"
+ 
+ input: "aabaaac"
+ output: "a2ba3c"
+ 
+ */
+
+
+func encodeString(str: String) -> String {
+    
+    var encodedStr: String = ""
+    var currCount = 0
+    var currChar: Character = "."
+    
+    for char in str {
+        if char != currChar {
+            // append to the encodedStr
+            if currCount > 1 {
+                encodedStr += (String(currChar) + String(currCount))
+            } else if currCount == 1 {
+                encodedStr += String(currChar)
+            }
+            
+            currChar = char
+            currCount = 1
+    
+        } else {
+            // increment the count
+            currCount += 1
+        }
+    }
+    
+    if currCount > 1 {
+        encodedStr += (String(currChar) + String(currCount))
+    } else if currCount == 1 {
+        encodedStr += String(currChar)
+    }
+    
+    
+    return encodedStr
+}
+encodeString(str: "aaabbcccc")
